3027062edd922b0a4528cee804662130b43345d9
Enrique <enrique.navarro@ingenieros.com>
2022-04-05 23:05:18 +0200

    Configuration DB, Logs in Application Properties

---
+++ src/main/java/com/yelko/app/apirest/controllers/ApiController.java
@@ -0,0 +1,20 @@
+package com.yelko.app.apirest.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/")
+public class ApiController {
+
+    @GetMapping("/example")
+    public ResponseEntity<Object> index(){
+        return ResponseEntity.ok("OK");
+    }
+
+
+}
